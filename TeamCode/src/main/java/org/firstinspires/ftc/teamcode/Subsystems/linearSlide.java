9cbfad973a27596bb83963234caa0810133f88ca
GosProgrammers <girlsofsteelrobotics@gmail.com>
2024-11-21 19:34:40 -0500

    added code for LinearSlide. Created clawSubsystem.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/linearSlide.java
@@ -0,0 +1,41 @@
+package org.firstinspires.ftc.teamcode.Subsystems;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+//package org.firstinspires.ftc.teamcode.subsystems;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+public class linearSlide {
+    private static final double TICKS_PER_INCH = (-2843 - -183 ) / 27.5;
+
+    public static final double GROUND_HEIGHT = 0;
+    public static final double LOW_RUNG = 15;
+    public static final double LOW_BASKET = 28;
+
+
+    private final DcMotor linearSlide;
+    //might not be final just make sure later
+
+    public linearSlide (HardwareMap hardwareMap) {
+        linearSlide = hardwareMap.get(DcMotor.class, "linearslide");
+        linearSlide.setDirection(DcMotorSimple.Direction.FORWARD);
+        //change "linearSlide" to the name that we configured on the phone, once charged
+    }
+
+    public void goUp() {
+        linearSlide.setPower(-1);
+    }
+
+    public void goDown() {
+        linearSlide.setPower(1);
+    }
+
+    public void stop() {
+        linearSlide.setPower(0);
+    }
+}
+
+
