3db4cb4532fcb1b8b4d97f3edba367835ab0323d
GosProgrammers <girlsofsteelrobotics@gmail.com>
2024-11-21 19:04:13 -0500

    created linearSlide class. Finished tuning.

--- MeepMeepTesting/src/main/java/com/gos/meepmeeptesting/Main.java
+++ MeepMeepTesting/src/main/java/com/gos/meepmeeptesting/Main.java
@@ -1,11 +1,9 @@
 package com.gos.meepmeeptesting;
 
 import com.acmerobotics.roadrunner.Pose2d;
-import com.acmerobotics.roadrunner.SleepAction;
 import com.acmerobotics.roadrunner.Vector2d;
 import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
-import com.noahbres.meepmeep.roadrunner.DriveShim;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
 public class Main {
@@ -20,25 +18,22 @@ public class Main {
         myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(-35, -61, Math.toRadians(-90)))
                 // START COPY AND PASTE
                 .setReversed(true)
-                .splineTo(new Vector2d(-35.5,-9), Math.toRadians(90))
-                .strafeTo(new Vector2d(-45,-9))
-                .strafeToConstantHeading(new Vector2d(-45,-55))
-                .strafeTo(new Vector2d(-60,-60))
-                .strafeTo(new Vector2d(-45, - 38))
+                .splineTo(new Vector2d(-35.5, -9), Math.toRadians(90))
                 .strafeTo(new Vector2d(-45, -9))
-                .strafeTo(new Vector2d(-56,-9))
-                .strafeTo(new Vector2d(-56,-60))
-                .strafeTo(new Vector2d(-56,-40))
-                .strafeTo(new Vector2d(-56,-8))
+                .strafeToConstantHeading(new Vector2d(-45, -55))
+                .strafeTo(new Vector2d(-60, -60))
+                .strafeTo(new Vector2d(-45, -38))
+                .strafeTo(new Vector2d(-45, -9))
+                .strafeTo(new Vector2d(-56, -9))
+                .strafeTo(new Vector2d(-56, -60))
+                .strafeTo(new Vector2d(-56, -40))
+                .strafeTo(new Vector2d(-56, -8))
                 .turn(Math.toRadians(-90))
-                .strafeTo(new Vector2d(-53,-8))
-                .strafeTo(new Vector2d(-53,-29))
-                .strafeTo(new Vector2d(-63,-20))
-                .strafeTo(new Vector2d(-63,-60))
+                .strafeTo(new Vector2d(-53, -8))
+                .strafeTo(new Vector2d(-53, -29))
+                .strafeTo(new Vector2d(-63, -20))
+                .strafeTo(new Vector2d(-63, -60))
                 // END COPY AND PASTE
-
-
-
 
 
 //                .turn(Math.toRadians(90))
